a493114ead89c09cf53248cab372d373b96e35e0
NickAllenov <AllenovNS@gmail.com>
2023-12-13 00:19:44 +0300

    guess_num

--- Seminar_6/guess_num.py
+++ Seminar_6/guess_num.py
@@ -2,7 +2,7 @@
 
 from random import randint
 
-def guess(lower: int = 0, upper: int = 100, attempt: int = 10):
+def guess(lower: int = 0, upper: int = 100, attempt: int = 10) -> bool:
     r_num = randint(lower, upper)
     for nn in range(1, attempt+1):
         answer = int(input(f'Попытка № {nn}, введите число от {lower} до {upper}:  '))
