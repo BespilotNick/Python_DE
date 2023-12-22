0ec20f37b8ed8c517d49fbbba54e990508c2c228
NickAllenov <AllenovNS@gmail.com>
2023-12-22 19:47:19 +0300

    Hometask_8(final)

--- Hometask_8_(final)/traversing_directory.py
+++ Hometask_8_(final)/traversing_directory.py
@@ -1,3 +1,10 @@
+# Напишите функцию, которая получает на вход директорию и рекурсивно обходит её и все вложенные директории.
+# Результаты обхода сохраните в файлы json, csv и pickle.
+# ○ Для дочерних объектов указывайте родительскую директорию.
+# ○ Для каждого объекта укажите файл это или директория.
+# ○ Для файлов сохраните его размер в байтах, а для директорий размер файлов в ней с учётом всех
+#   вложенных файлов и директорий.
+
 import os
 from pathlib import Path
 import json
@@ -7,8 +14,9 @@ import pickle
 def traversing_directory(directory: str | Path) -> None:
 
     res_dict = {}
-    for dir_path, dir_file, file_name in os.walk(directory):
-        res_dict[f'DIRECTORY - {dir_path}'] = [
+    for dir_path, dir_file, file_name in os.walk(top=directory):
+        res_dict[f'DIRECTORY - {dir_path}, size = {sum(os.path.getsize(os.path.join(dir_path, name)) 
+                                                       for name in file_name)}'] = [
             f'FILE - {i} = {os.path.getsize(os.path.abspath(dir_path + "/" + i))} byte' for i in file_name]
     with open('traversed_directory.json', 'w', encoding='utf-8') as fjson_write:
         json.dump(res_dict, fjson_write, indent=2, ensure_ascii=False, separators=(',', ':'))
