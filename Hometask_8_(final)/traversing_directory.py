d2441e82a6c8f4aba8360f4f3870666dcd48cd18
NickAllenov <AllenovNS@gmail.com>
2023-12-22 19:34:18 +0300

    Hometask_8(final)

--- Hometask_8_(final)/traversing_directory.py
+++ Hometask_8_(final)/traversing_directory.py
@@ -1,47 +1,28 @@
-# Напишите функцию, которая получает на вход директорию и рекурсивно обходит её и все вложенные директории.
-# Результаты обхода сохраните в файлы json, csv и pickle.
-# ○ Для дочерних объектов указывайте родительскую директорию.
-# ○ Для каждого объекта укажите файл это или директория.
-# ○ Для файлов сохраните его размер в байтах, а для директорий размер файлов в ней с учётом всех
-#   вложенных файлов и директорий.
-
-
 import os
 from pathlib import Path
 import json
 import csv
 import pickle
 
+def traversing_directory(directory: str | Path) -> None:
 
-def traversing_directory(directory: Path) -> None:
-    os.chdir(directory)
     res_dict = {}
-    res_dict[{os.getcwd()}] = {}
-    for dir_path, dir_name, file_name in os.walk(os.getcwd()):
-        print(dir_path, dir_name, file_name)
-        for i in dir_name:
-            print(os.path.getsize(Path(i)))
-            # for j in file_name:
-            #     print(os.path.getsize(Path(j)))
+    for dir_path, dir_file, file_name in os.walk(directory):
+        res_dict[f'DIRECTORY - {dir_path}'] = [
+            f'FILE - {i} = {os.path.getsize(os.path.abspath(dir_path + "/" + i))} byte' for i in file_name]
+    with open('traversed_directory.json', 'w', encoding='utf-8') as fjson_write:
+        json.dump(res_dict, fjson_write, indent=2, ensure_ascii=False, separators=(',', ':'))
 
-    # print(res_dict)
+    with open('traversed_directory.pickle', 'wb') as fpick_write:
+        pickle.dump(res_dict, fpick_write)
 
-    # with open('traversed_directory.json', 'w', encoding='utf-8') as fjson_write:
-    #     json.dump(res_dict, fjson_write, indent=2, ensure_ascii=False)
-    #
-    # with open('traversed_directory.pickle', 'wb') as fpick_write:
-    #     pickle.dump(res_dict, fpick_write)
-    #
-    # list_rows = []
-    # with open('traversing_directory.csv', 'w', newline='', encoding='utf-8') as fcsv_write:
-    #     csv_write = csv.DictWriter(fcsv_write,
-    #                                fieldnames=['Main Directory', 'Parent Directory', 'Object_name', 'Type', 'Size'],
-    #                                dialect='excel-tab')
-    #     csv_write.writeheader()
-    #     csv_write.writerow(list_rows)
+    data = [["Dir", "Files"]]
+    for key, value in res_dict.items():
+        data.append([key, value])
+    with open('traversed_directory.csv', 'w', encoding='utf-8') as csv_f:
+        write_csv = csv.writer(csv_f, dialect='excel-tab', delimiter=',')
+        write_csv.writerows(data)
 
 
 
-
-if __name__ == '__main__':
-    traversing_directory(Path('D:\\GeekBrains\\DATA ENGINEER\\Python DE\\Seminar_7'))
+traversing_directory('D:\\GeekBrains\\DATA ENGINEER\\Python DE\\Hometask_8_(final)\\Copy_S_7_for_test')
